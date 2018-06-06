2550c4dd294e71d37d4226c088688397bb293ac7
ike lee <ikelee@ikes-MacBook-Pro.local>
2018-06-05 23:29:26 -0400

    Deploy to Heroku

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@ TIMEOUT=120
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
+echo process.env.PORT
 exec gunicorn lunchdate.wsgi:application \
-    --bind 0.0.0.0:8080 \
+    --bind process.env.PORT \
     --workers 3 \
     --timeout $TIMEOUT
